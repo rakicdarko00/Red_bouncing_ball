ced4b97d04cf8169dd054acd0d50a2720c97794f
dejanar <aleksandar.rapaic@gmail.com>
2018-05-14 15:40:52 +0200

    added map

--- battle_city_design/SDK/SDK_Workspace/super_mario/src/map.h
+++ battle_city_design/SDK/SDK_Workspace/super_mario/src/map.h
@@ -2,5 +2,9 @@
 #define _MAP_H_
 
 unsigned char  map1[30][160];
+unsigned char  map2[30][160];
+unsigned char  map3[30][160];
+unsigned char  map_win[30][160];
+unsigned char  map_game_over[30][160];
 
 #endif // _MAP_H_
